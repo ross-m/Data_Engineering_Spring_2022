8bc63dba92a80282b8a75bbfd30ba2e5ead56a9c
ross-m <rossmorrison53@yahoo.com>
2022-05-25 16:11:40 -0700

    week 9 in class

---
+++ week9/consumer2.py
@@ -0,0 +1,44 @@
+from confluent_kafka import Consumer
+from google.cloud import storage
+from datetime import date, timedelta
+import json
+import sys
+import ccloud_lib
+
+if __name__ == '__main__':
+    # Read arguments and configurations and initialize
+    args = ccloud_lib.parse_args()
+    config_file = args.config_file
+    topic = args.topic
+    conf = ccloud_lib.read_ccloud_config(config_file)
+
+    # Create Consumer instance
+    # 'auto.offset.reset=earliest' to start reading from the beginning of the
+    #   topic if no committed offsets exist
+    consumer_conf = ccloud_lib.pop_schema_registry_params_from_config(conf)
+    consumer_conf['group.id'] = 'python_example_group_1'
+    consumer_conf['auto.offset.reset'] = 'earliest'
+    consumer = Consumer(consumer_conf)
+
+    # Subscribe to topic
+    consumer.subscribe([topic]) 
+    storage_client = storage.Client()
+    bucket = storage_client.bucket('week-9')
+	
+    try:
+        while True:
+            msg = consumer.poll(1)
+            if msg is None:
+		continue 
+            elif msg.error() or msg.value() is None:
+                print('error: {}'.format(msg.error()), file=sys.stdout)
+            else:
+                # Check for Kafka message, add to dataframe
+		blob = bucket.blob(str(total_count))
+		blob.upload_from_string(msg.value())
+                total_count += 1
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # Leave group and validate+reshape data
+        consumer.close()
